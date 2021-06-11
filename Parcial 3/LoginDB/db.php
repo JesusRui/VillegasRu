98f021bdde5032f4bf748b0f6396c2480015d543
JesusRui <78823649+JesusRui@users.noreply.github.com>
2021-06-11 18:56:23 -0500

    Add files via upload

---
+++ Parcial 3/LoginDB/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$usuario  = "root";
+$password = "";
+$servidor = "localhost";
+$basededatos = "villegasru";
+$con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
+mysqli_query($con,"SET SESSION collation_connection ='utf8_unicode_ci'");
+$db = mysqli_select_db($con, $basededatos) or die("Error en conectar a la Base de Datos");
+
+
+?>
+<H1>CONECTADO</H1>
