f773a31e1c6de0da02808d76459634670b379e71
JesusRui <78823649+JesusRui@users.noreply.github.com>
2021-06-11 18:52:28 -0500

    Update BaseWeb.sql

--- Parcial 3/Ejercicio 1/BaseWeb.sql
+++ Parcial 3/Ejercicio 1/BaseWeb.sql
@@ -1,16 +1,38 @@
 CREATE DATABASE villegasru
 use villegasru;
 
-CREATE TABLE `villegasru`.`clientes` (
-  `idclientes` INT NOT NULL,
-  `Nombre` VARCHAR(45) NULL,
-  `Apellido` VARCHAR(45) NULL,
-  `NumeroTel` VARCHAR(45) NULL,
-  PRIMARY KEY (`idclientes`));
+CREATE TABLE `villegasru`.`usuarios` (
+  `idusuarios` INT NOT NULL,
+  `usuario` VARCHAR(45) NULL,
+  `contraseña` VARCHAR(45) NULL,
+  PRIMARY KEY (`idusuarios`));
 
-INSERT INTO `villegasru`.`clientes` (`idclientes`, `Nombre`, `Apellido`, `NumeroTel`) VALUES ('1', 'Jesus', 'Villegas', '123');
-INSERT INTO `villegasru`.`clientes` (`idclientes`, `Nombre`, `Apellido`, `NumeroTel`) VALUES ('2', 'Oziel', 'Benavides', '321');
-INSERT INTO `villegasru`.`clientes` (`idclientes`, `Nombre`, `Apellido`, `NumeroTel`) VALUES ('3', 'Rosita ', 'Montes', '789');
-INSERT INTO `villegasru`.`clientes` (`idclientes`, `Nombre`, `Apellido`, `NumeroTel`) VALUES ('4', 'Juan ', 'Hernandez', '456');
-INSERT INTO `villegasru`.`clientes` (`idclientes`, `Nombre`, `Apellido`, `NumeroTel`) VALUES ('5', 'Leo', 'Messi', '987');
+INSERT INTO `villegasru`.`usuarios` (`idusuarios`, `usuario`, `contraseña`) VALUES ('1', 'jesus', 'villegas');
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `correo` varchar(250) NOT NULL,
+  `celular` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `clientes` (`id`, `nombre`, `correo`, `celular`) VALUES
+(680, 'Alex puti', 'alexsarriahenao.2307@gmail.com', '3112781478'),
+(682, 'Adriana', 'amilenita64@hotmail.com', '3176802693'),
+(684, 'DAMARIS', 'damaris-rincon@hotmail.com', '3115050103'),
+(685, 'Magaly', 'magaly_vega87@yahoo.com', '3014378134'),
+(686, 'Oscar', 'oscarbogota79@gmail.com', '3162655524'),
+(687, 'Francy', 'francymesa@gmail.com', '3212060512'),
+(688, 'ADRIANA MERCEDES', 'adrimher_99@hotmail.com', '3108039620'),
+(690, 'Fredy', 'fbejarano1982@yahoo.es', '3142159522'),
+(692, 'JAVIER ', 'javierdario.0870@gmail.com', '3124861910'),
+(693, 'JAIRO NEL ', 'jaironel2007@gmail.com', '3209642982'),
+(695, 'Marcos', 'aldanilly@yahoo.com.co', '3102418894');
+
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `clientes`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2000;
+COMMIT;
 
